16b6883daff8705254c4b828bc132929a832f95b
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2015-06-20 13:09:20 +0530

    Urls Fix
    * removes DeveloperProjectFormView url from urls.py

--- properties/urls.py
+++ properties/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import url
 from views import (
     BasicDetailsFormView,
     DashboardView,
-    DeveloperProjectFormView)
+)
 
 urlpatterns = [
-    url(r'^project/$', DeveloperProjectFormView.as_view(), name='project'),
     url(r'^basic/$', BasicDetailsFormView.as_view(), name='basic'),
     url(r'^dashboard/(?P<property_id>[0-9]+)/$', DashboardView.as_view(),
         name='dashboard'),
