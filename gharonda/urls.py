ca44b0a071bd773e7f651240589bb935d114c3cb
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2015-07-09 16:06:51 +0530

    All URL's changed

--- gharonda/urls.py
+++ gharonda/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.conf import settings
 
+from custom_user.views import IndexView
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('custom_user.urls')),
-    url(r'^properties/', include('properties.urls')),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^property/', include('properties.urls')),
+    url(r'^user/', include('custom_user.urls')),
     url(r'^ajaximage/', include('ajaximage.urls')),
 ]
 
--- properties/urls.py
+++ properties/urls.py
@@ -9,13 +9,13 @@ from views import (
     PropertyListView)
 
 urlpatterns = [
-    url(r'^basic/$', BasicDetailsFormView.as_view(), name='basic'),
+    url(r'^add/$', BasicDetailsFormView.as_view(), name='basic'),
     url(r'^search/$', SearchView.as_view(), name='search'),
-    url(r'^property/(?P<city_slug>[\w-]+)/(?P<project_id>[0-9]+)/$',
-        PropertyListView.as_view(), name='property-list'),
-    url(r'^property/(?P<pk>[0-9]+)/$', PropertyDetailView.as_view(),
-        name='property-detail'),
-    url(r'^dashboard/property_filter/$', city_filter, name='city-filter'),
-    url(r'^dashboard/(?P<property_id>[0-9]+)/$', DashboardView.as_view(),
+    url(r'^edit/(?P<property_id>[0-9]+)/$', DashboardView.as_view(),
         name='dashboard'),
+    url(r'^(?P<city_slug>[\w-]+)/(?P<project_id>[0-9]+)/$',
+        PropertyListView.as_view(), name='property-list'),
+    url(r'^(?P<pk>[0-9]+)/$', PropertyDetailView.as_view(),
+        name='property-detail'),
+    url(r'^property_filter/$', city_filter, name='city-filter'),
 ]
