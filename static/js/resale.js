da95f23a0b2d8dfb70568dcd1cf755962c7b6849
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2015-08-07 00:56:03 +0530

    Fix resale.js to show fields only when re-sale is selected

--- static/js/resale.js
+++ static/js/resale.js
@@ -1,25 +1,25 @@
 function resaleCheck() {
-    $("#id_is_resale_1").click(function () {
+    $("#id_is_resale_2").click(function () {
 
         $('#div_id_name_of_seller').show(500);
         $('#div_id_contact_number_seller').show(500);
         $('#div_id_email_seller').show(500);
     });
 
-    $("#id_is_resale_2").click(function () {
+    $("#id_is_resale_1").click(function () {
 
         $('#div_id_name_of_seller').hide(500);
         $('#div_id_contact_number_seller').hide(500);
         $('#div_id_email_seller').hide(500);
     });
 
-    if ($('#id_is_resale_1').is(":checked")) {
+    if ($('#id_is_resale_2').is(":checked")) {
         $('#div_id_name_of_seller').show(500);
         $('#div_id_contact_number_seller').show(500);
         $('#div_id_email_seller').show(500);
     }
 
-    if ($('#id_is_resale_2').is(":checked")) {
+    if ($('#id_is_resale_1').is(":checked")) {
         $('#div_id_name_of_seller').hide(500);
         $('#div_id_contact_number_seller').hide(500);
         $('#div_id_email_seller').hide(500);
