bc71feec9bddea9cb3f1ebf68abea1fd44dc9874
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2015-09-26 13:35:57 +0530

    Adds Import/Export csv for every model

--- properties/admin.py
+++ properties/admin.py
@@ -5,16 +5,6 @@ from import_export.admin import ImportExportModelAdmin
 from models import Property, Developer, DeveloperProject, Owner, Bank, \
     Permissions, Project, Tower, ProjectPermission, State, City, PinCode
 
-admin.site.register(Property)
-admin.site.register(Developer)
-admin.site.register(DeveloperProject)
-admin.site.register(Owner)
-admin.site.register(Permissions)
-admin.site.register(Tower)
-admin.site.register(State)
-admin.site.register(City)
-admin.site.register(PinCode)
-
 
 class BankResource(resources.ModelResource):
     class Meta:
@@ -27,6 +17,114 @@ class BankAdmin(ImportExportModelAdmin):
 
 
 admin.site.register(Bank, BankAdmin)
+
+
+class CityResource(resources.ModelResource):
+    class Meta:
+        model = City
+
+
+class CityAdmin(ImportExportModelAdmin):
+    resource_class = CityResource
+
+
+admin.site.register(City, CityAdmin)
+
+
+class DeveloperResource(resources.ModelResource):
+    class Meta:
+        model = Developer
+
+
+class DeveloperAdmin(ImportExportModelAdmin):
+    resource_class = DeveloperResource
+
+
+admin.site.register(Developer, DeveloperAdmin)
+
+
+class PinCodeResource(resources.ModelResource):
+    class Meta:
+        model = PinCode
+
+
+class PinCodeAdmin(ImportExportModelAdmin):
+    resource_class = PinCodeResource
+
+
+admin.site.register(PinCode, PinCodeAdmin)
+
+
+class StateResource(resources.ModelResource):
+    class Meta:
+        model = State
+
+
+class StateAdmin(ImportExportModelAdmin):
+    resource_class = StateResource
+
+
+admin.site.register(State, StateAdmin)
+
+
+class TowerResource(resources.ModelResource):
+    class Meta:
+        model = Tower
+
+
+class TowerAdmin(ImportExportModelAdmin):
+    resource_class = TowerResource
+
+
+admin.site.register(Tower, TowerAdmin)
+
+
+class PermissionsResource(resources.ModelResource):
+    class Meta:
+        model = Permissions
+
+
+class PermissionAdmin(ImportExportModelAdmin):
+    resource_class = PermissionsResource
+
+
+admin.site.register(Permissions, PermissionAdmin)
+
+
+class OwnerResource(resources.ModelResource):
+    class Meta:
+        model = Owner
+
+
+class OwnerAdmin(ImportExportModelAdmin):
+    resource_class = OwnerResource
+
+
+admin.site.register(Owner, OwnerAdmin)
+
+
+class DeveloperProjectResource(resources.ModelResource):
+    class Meta:
+        model = DeveloperProject
+
+
+class DeveloperProjectAdmin(ImportExportModelAdmin):
+    resource_class = DeveloperProjectResource
+
+
+admin.site.register(DeveloperProject, DeveloperProjectAdmin)
+
+
+class PropertyResource(resources.ModelResource):
+    class Meta:
+        model = Property
+
+
+class PropertyAdmin(ImportExportModelAdmin):
+    resource_class = PropertyResource
+
+
+admin.site.register(Property, PropertyAdmin)
 
 
 class ProjectPermissionResource(resources.ModelResource):
